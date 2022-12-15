65b65c12103c46de3d54328c5baf8302b37dd45d
shintarou.chou <shintarou.chou@gmail.com>
2022-12-15 16:35:50 +0900

    PropsDemo

---
+++ src/components/PropsDemo.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: shintarou.chou shintarou.chou@gmail.com
+ * @Date: 2022-12-15 16:14:44
+ * @LastEditors: shintarou.chou shintarou.chou@gmail.com
+ * @LastEditTime: 2022-12-15 16:32:46
+ * @FilePath: /vue-demo-1/src/components/PropsDemo.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div class="blue">
+    这里是PropsDemo内部
+    {{ message }}
+    <button @click="fn">call fn</button>
+  </div>
+</template>
+<script>
+export default {
+  props: ["message", "fn"],
+};
+</script>
+
+<style scoped>
+.blue {
+  color: blue;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,28 +2,34 @@
  * @Author: shintarou.chou shintarou.chou@gmail.com
  * @Date: 2022-10-25 17:34:51
  * @LastEditors: shintarou.chou shintarou.chou@gmail.com
- * @LastEditTime: 2022-12-15 16:07:10
+ * @LastEditTime: 2022-12-15 16:35:10
  * @FilePath: /vue-demo-1/src/main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import Demo from "./components/LifeCycleDemo.vue";
+import LifeCycleDemo from "./components/LifeCycleDemo.vue";
+import PropsDemo from "./components/PropsDemo.vue";
 const Vue = window.Vue;
 
 Vue.config.productionTip = false;
 
 new Vue({
-  components: { Demo },
+  components: { LifeCycleDemo, PropsDemo },
   data: {
     visible: true,
   },
   template: `
   <div>
   <button @click="toggle">toggle</button>
-  <hr><Demo v-if="visible===true"></Demo></div>
+  <hr><LifeCycleDemo v-if="visible===true"></LifeCycleDemo>
+  <PropsDemo :fn="toggle"/>
+  <PropsDemo :message="this.visible" :fn="toggle"/>
+  </div>
+
   `,
   methods: {
     toggle() {
       this.visible = !this.visible;
+      console.log("called");
     },
   },
 }).$mount("#app");
